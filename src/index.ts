68fbe45f6c859db4c5d6cd8e9c9cf148aac545d6
Christopher Eadie <caeadie@hotmail.com>
2023-12-14 12:31:26 +0000

    added cors to api

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import 'dotenv/config'
 import express from 'express';
 import router from './router';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.disable('x-powered-by');
+
+const corsOptions = {
+    origin:[process.env.URI_DEVELOPMENT as string,process.env.URI_PRODUCTION as string]
+}
+app.use(cors(corsOptions))
 
 app.get('/', (req,  res) => {
     res.status(200).json({message: 'hello'})
